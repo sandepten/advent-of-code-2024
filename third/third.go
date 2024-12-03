91ed86417e798aef251a6274fea963c6e0e185e8
Sandeep Kumar <sandepten@gmail.com>
2024-12-03 11:15:58 +0530

    third day part 2

--- third/third.go
+++ third/third.go
@@ -18,21 +18,31 @@ func main() {
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
+	shouldMultipleDoCondition := true
 	for fileScanner.Scan() {
-		totalSum += totalSumForLine(fileScanner.Text())
+		totalSum += totalSumForLine(fileScanner.Text(), &shouldMultipleDoCondition)
 	}
 	fmt.Println(totalSum)
 
 	readFile.Close()
 }
 
-func totalSumForLine(line string) int {
+func totalSumForLine(line string, shouldMultipleDoCondition *bool) int {
 	totalSum := 0
 	mulRegex := `^mul\((-?\d+),(-?\d+)\)$`
 	regex := regexp.MustCompile(mulRegex)
 
 	subStrings := getConsecutiveSubstrings(line)
 	for _, v := range subStrings {
+		if v == "don't()" {
+			*shouldMultipleDoCondition = false
+		}
+		if v == "do()" {
+			*shouldMultipleDoCondition = true
+		}
+		if !*shouldMultipleDoCondition {
+			continue
+		}
 		if matches := regex.FindStringSubmatch(v); matches != nil {
 			// Extract the numbers
 			num1, _ := strconv.Atoi(matches[1])
