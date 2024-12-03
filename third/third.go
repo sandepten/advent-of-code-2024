e69fde0d02dc474d53b7bfd9e6672456781d20e1
Sandeep Kumar <sandepten@gmail.com>
2024-12-03 11:04:27 +0530

    third day part 1

---
+++ third/third.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	totalSum := 0
+	readFile, err := os.Open("thirdInput.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		totalSum += totalSumForLine(fileScanner.Text())
+	}
+	fmt.Println(totalSum)
+
+	readFile.Close()
+}
+
+func totalSumForLine(line string) int {
+	totalSum := 0
+	mulRegex := `^mul\((-?\d+),(-?\d+)\)$`
+	regex := regexp.MustCompile(mulRegex)
+
+	subStrings := getConsecutiveSubstrings(line)
+	for _, v := range subStrings {
+		if matches := regex.FindStringSubmatch(v); matches != nil {
+			// Extract the numbers
+			num1, _ := strconv.Atoi(matches[1])
+			num2, _ := strconv.Atoi(matches[2])
+
+			totalSum += num1 * num2
+		}
+	}
+
+	return totalSum
+}
+
+func getConsecutiveSubstrings(s string) []string {
+	// If the string is empty, return an empty slice
+	if len(s) == 0 {
+		return []string{}
+	}
+
+	substrings := []string{}
+
+	for start := 0; start < len(s); start++ {
+		for end := start + 1; end <= len(s); end++ {
+			substrings = append(substrings, s[start:end])
+		}
+	}
+
+	return substrings
+}
