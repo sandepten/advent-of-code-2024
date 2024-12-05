9b865f4e523725b277c086bfdf8e6b6a95c1826d
Sandeep Kumar <sandepten@gmail.com>
2024-12-05 16:18:17 +0530

    forth day part 1

--- forth/forth.go
+++ forth/forth.go
@@ -28,11 +28,78 @@ func main() {
 		xmasMatrix = append(xmasMatrix, lineArray)
 		lineNumber++
 	}
+	totalCount += countHorizontal(xmasMatrix)
+	totalCount += countVertical(xmasMatrix)
+	totalCount += countDiagonal(xmasMatrix)
 	fmt.Println(totalCount)
 
 	readFile.Close()
 }
 
-func countHorizontal(xmasMatrix [][]string) {
+func countHorizontal(xmasMatrix [][]string) int {
+	xmasCount := 0
+	for i := 0; i < len(xmasMatrix); i++ {
+		for j := 0; j < len(xmasMatrix[i])-3; j++ {
+			currWord := ""
+			for k := j; k < j+4; k++ {
+				currWord += xmasMatrix[i][k]
+			}
+			if currWord == "XMAS" || currWord == "SAMX" {
+				xmasCount++
+			}
+		}
+	}
 
+	return xmasCount
+}
+
+func countVertical(xmasMatrix [][]string) int {
+	xmasCount := 0
+
+	for i := 0; i < len(xmasMatrix[0]); i++ {
+		for j := 0; j < len(xmasMatrix)-3; j++ {
+			currWord := ""
+			for k := j; k < j+4; k++ {
+				currWord += xmasMatrix[k][i]
+			}
+			if currWord == "XMAS" || currWord == "SAMX" {
+				xmasCount++
+			}
+		}
+	}
+
+	return xmasCount
+}
+
+func countDiagonal(xmasMatrix [][]string) int {
+	xmasCount := 0
+
+	for i := 0; i < len(xmasMatrix)-3; i++ {
+		for j := 0; j < len(xmasMatrix[i]); j++ {
+			currWord := ""
+
+			// left Diagonal
+			if j > 2 {
+				for k := 0; k < 4; k++ {
+					currWord += xmasMatrix[i+k][j-k]
+				}
+			}
+			if currWord == "XMAS" || currWord == "SAMX" {
+				xmasCount++
+			}
+
+			// // right Diagonal
+			currWord = ""
+			if j < len(xmasMatrix[i])-3 {
+				for k := 0; k < 4; k++ {
+					currWord += xmasMatrix[i+k][j+k]
+				}
+			}
+			if currWord == "XMAS" || currWord == "SAMX" {
+				xmasCount++
+			}
+		}
+	}
+
+	return xmasCount
 }
